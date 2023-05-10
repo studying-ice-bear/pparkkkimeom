f2f6317acdc73687d62359ed087c1900b76f3d51
elizabethgim <yujingim@naver.com>
2023-05-10 18:04:43 +0900

    [프로그래머스, Lv.2] 게임 맵 최단거리, 다른 풀이

--- GimYujin/Programmers/BFS_DFS/게임_맵_최단거리.py
+++ GimYujin/Programmers/BFS_DFS/게임_맵_최단거리.py
@@ -1,4 +1,35 @@
 from collections import deque
+def solution2(maps):
+    answer = 0
+    n, m = len(maps), len(maps[0])
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    visited = [[False for _ in range(m)] for _ in range(n)]
+
+    queue = deque()
+    queue.append((n-1, m-1))
+    visited[n-1][m-1] = True
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if not visited[nx][ny] and maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+
+    answer = maps[0][0]
+    if maps[0][0] == 1:
+        answer = -1
+
+    return answer
+
 
 def solution(maps):
     answer = 0
