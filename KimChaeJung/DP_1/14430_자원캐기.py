4a0b0a1b36f79d1d6a39e2b0f663e9eb88998c66
blcklamb <whenucan35@gmail.com>
2023-05-16 15:16:35 +0900

    [백준, 14430] 자원캐기

---
+++ KimChaeJung/DP_1/14430_자원캐기.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/14430
+N, M = map(int, input().split())
+area = []
+summedArea = []
+for i in range(N):
+    row = list(map(int, input().split()))
+    if i == 0:
+        firstRowValue = 0
+        for idx in range(M):
+            firstRowValue += row[idx]
+            row[idx] = firstRowValue
+    summedArea.append(row)
+
+for row in range(1, N):
+    newSummedRow = []
+    for idx in range(M):
+        prevRowValue = summedArea[row-1][idx]
+        currentValue = summedArea[row][idx]
+        if idx == 0:
+            newSummedRow.append(
+                prevRowValue+currentValue)
+        else:
+            prevValue = newSummedRow[idx-1]
+            newSummedRow.append(
+                max(prevRowValue, prevValue)+currentValue)
+    summedArea[row] = newSummedRow
+
+print(summedArea[N-1][M-1])
