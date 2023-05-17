0f938007ca6a6fa832d09504aef03493ae500de2
elizabethgim <yujingim@naver.com>
2023-05-18 00:36:36 +0900

    [백준, 15565] 귀여운 라이언, 투 포인터 사용한 풀이

--- GimYujin/15565_귀여운_라이언.py
+++ GimYujin/15565_귀여운_라이언.py
@@ -1,26 +1,46 @@
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
-dp = []
+start, end = 0, 0
+answer = 1000001
+cnt = 0
 
-for i in range(N):
-    if arr[i] == 1:
-        dp.append(i)
+for start in range(N):
+    while end < N:
+        if cnt == K:
+            break
 
-if len(dp) < K:
-    answer = -1
+        if arr[end] == 1:
+            cnt += 1
+
+        end += 1
+
+    if cnt == K and answer > end - start:
+        answer = end - start
+
+    if arr[start] == 1:
+        cnt -= 1
+
+if answer == 1000001:
+    print(-1)
 else:
-    answer = 1000001
-
-    for i in range(len(dp)):
-        if i + K > len(dp):
-            continue
-        answer = min(answer, dp[i+K-1] - dp[i]+1)
-
-print(answer)
+    print(answer)
 
 '''
+10 3
+1 2 2 2 1 2 1 2 2 1
+6
+
 5 2
 1 1 2 2 1
+2
+
+10 3
+1 1 1 2 1 2 1 2 2 1
+3
+
+반례
+10 2
+2 2 2 1 1 2 2 2 2 2
 2
 '''
