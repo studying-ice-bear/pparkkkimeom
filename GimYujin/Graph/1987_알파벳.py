3d9dc489bb8ccd78e1c39d1f9b70459f744a2467
elizabethgim <yujingim@naver.com>
2023-06-22 21:59:36 +0900

    [백준, 1987] 알파벳, 다른 풀이

--- GimYujin/Graph/1987_알파벳.py
+++ GimYujin/Graph/1987_알파벳.py
@@ -3,11 +3,15 @@ input = sys.stdin.readline
 R, C = map(int, input().split())
 board = []
 for _ in range(R):
-    string = input()
+    string = input().strip()
     board.append(list(s for s in string))
+
+# graph = [list(map(str, sys.stdin.readline().strip())) for _ in range(R)]
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
+
+'''dfs 풀이, pypy로 통과
 
 answer = 0
 max_answer = 0
@@ -33,34 +37,25 @@ def dfs(x, y):
 
 dfs(0, 0)
 print(max_answer)
-
 '''
-from collections import deque
-
-def bfs(start):
-    que = deque()
-    que.append(start)
-    alphabet = [False for _ in range(26)]   # 65: A
-    alphabet[ord(board[start[0]][start[1]])-65] = True
-    visited = [[0 for _ in range(C)] for _ in range(R)]
-    answer = 0
+answer = 1
+# bfs 풀이?(python3): https://fre2-dom.tistory.com/245
+def bfs():
+    global answer
+    que = set([(0, 0, board[0][0])])
 
     while que:
-        x, y, z = que.popleft()
-        answer += 1
+        x, y, z = que.pop()
+        answer = max(answer, len(z))
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < R and 0 <= ny < C:
-                j = ord(board[nx][ny]) - 65
-                if not alphabet[j]:
-                    alphabet[j] = True
-                    que.append((nx, ny))
+            if 0 <= nx < R and 0 <= ny < C and board[nx][ny] not in z:
+                que.add((nx, ny, board[nx][ny]+z))
 
     return answer
 
 
-print(bfs((0, 0)))
-'''
+print(bfs())
