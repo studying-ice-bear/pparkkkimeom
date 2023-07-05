074d15dc3989e569617a55aec21999c5ce0200e0
blcklamb <whenucan35@gmail.com>
2023-07-05 16:24:19 +0900

    [백준, 1987] 알파벳 - pypy로 시간초과 해결

--- KimChaeJung/backTracking/1987_알파벳.py
+++ KimChaeJung/backTracking/1987_알파벳.py
@@ -1,5 +1,7 @@
 # https://www.acmicpc.net/problem/1987
 # 시간초과
+
+# pypy 6944 ms
 import sys
 input = sys.stdin.readline
 R, C = map(int, input().split())
@@ -13,16 +15,24 @@ for r in range(R):
 
     boardInfo[r] = list(map(convertToNum, row))
 
+
+'''
+alpha라는 리스트를 backtracking 인자로 넘겨 append, pop -> 시간초과
+alpha를 문자열로 관리하지 않고 list index로 관리하기 위해 ord mapping 추가
+26일 때 더 이상 가지 않고 Return -> 시간초과
+count 대신 maxAlphaCount로 일일이 더하기 -> pypy로 겨우 통과
+'''
 maxAlphaCount = 0
+alphaCount = 0
 
 visitedNum = [0 for _ in range(26)]
 visitedNum[boardInfo[0][0]] = 1
 
 
 def backtracking(r, c):
-    global visitedNum, boardInfo, maxAlphaCount
-
-    maxAlphaCount = max(visitedNum.count(1), maxAlphaCount)
+    global visitedNum, boardInfo, maxAlphaCount, alphaCount
+    alphaCount += 1
+    maxAlphaCount = max(alphaCount, maxAlphaCount)
 
     if maxAlphaCount == 26:
         return
@@ -44,6 +54,7 @@ def backtracking(r, c):
             visitedNum[nextAlpha] = 1
 
             backtracking(nextR, nextC)
+            alphaCount -= 1
 
             visitedNum[nextAlpha] = 0
 
