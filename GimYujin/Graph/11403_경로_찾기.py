84d6e6868162a2ac00b4c712f4a89d3c1211afb4
elizabethgim <yujingim@naver.com>
2023-07-06 17:38:09 +0900

    [백준, 11403] 경로 찾기

---
+++ GimYujin/Graph/11403_경로_찾기.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+N = int(input())
+graph = [[] for _ in range(N+1)]
+for i in range(1, N+1):
+    data = list(map(int, input().split()))
+    for j in range(1, N+1):
+        if data[j-1] == 1:
+            graph[i].append(j)
+
+answer = [[0 for _ in range(N)] for _ in range(N)]
+visited = [False for _ in range(N+1)]
+# def dfs(check, node):
+#     visited[node] = True
+#     for i in graph[node]:
+#         if not visited[i]:
+#             visited[node] = True
+#             answer[check][node-1] = 1
+#             dfs(check, i)
+#     return
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visited = [False for _ in range(N+1)]
+
+    while queue:
+        x = queue.popleft()
+        for node in graph[x]:
+            if not visited[node]:
+                visited[node] = True
+                queue.append(node)
+                answer[start-1][node-1] = 1
+
+for i in range(1, N+1):
+    # bfs(i)
+    dfs(i, i)
+
+for ans in answer:
+    print(*ans)
